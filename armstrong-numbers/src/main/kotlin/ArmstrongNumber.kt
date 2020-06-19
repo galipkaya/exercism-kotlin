27ff44a8b751a02e4ef439ddec0ab5882293f922
Galip Kaya <galip.kaya@udemy.com>
2020-06-19 12:45:03 +0300

    Solution added

--- armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
+++ armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
@@ -1,7 +1,16 @@
+import javax.xml.stream.events.Characters
+import kotlin.math.pow
+
 object ArmstrongNumber {
 
     fun check(input: Int): Boolean {
-        TODO("Implement the function to complete the task")
+        val strNum = input.toString()
+        val power= strNum.length
+        val res = input.toString().fold(0){ sum, digit ->
+            println("d: $digit, s:$sum")
+            sum+Character.getNumericValue(digit).toFloat().pow(power).toInt()
+        }
+        return res == input
     }
 
 }
